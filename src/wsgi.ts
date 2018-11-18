a10f0dfffd4fd1a19f45ce55977ef1d7c1924fb6
jacktan <jacktan.whu@foxmail.com>
2018-11-18 20:32:00 +0800

    docs:wsgi:从PEP3333原文中摘录部分描述作为接口文档

--- src/wsgi.ts
+++ src/wsgi.ts
@@ -1,3 +1,7 @@
+/**
+ * [WSGI Specification](https://www.python.org/dev/peps/pep-3333/) described using TS
+ */
+
 type HeaderEntity = [string, string]
 
 interface ErrType{}
@@ -5,6 +9,11 @@ interface Err{}
 interface Traceback{}
 type ExcInfo = [ErrType, Err, Traceback]
 
+
+/**
+ * Must contain CGI-defined variables + WSGI-defined variables
+ * May also contain server-defined variables
+ */
 interface Environ {
   'REQUEST_METHOD': string,
   'SCRIPT_NAME': string,
@@ -21,16 +30,29 @@ interface Environ {
 }
 
 
+/**
+ * - It accepts a single parameter: a string to be written as part of the HTTP response body
+ * - It is returned by the `start_response()` callable
+ */
 interface Write {
   (data: string): void
 }
 
 
+/**
+ * - The `status` argument is an HTTP "status" string like "200 OK" or "404 Not Found"
+ * - The `response_headers` argument is a list of `(header_name, header_value)` tuples
+ * - The `exc_info argument`, if supplied, must be a `sys.exc_info()` tuple
+ * - It must return a `write(body_data)` callable
+ */
 interface StartResponse {
   (status: string, response_headers: HeaderEntity[], exc_info?: ExcInfo): Write
 }
 
 
+/**
+ * - An application __must__ return an iterable object
+ */
 interface WSGIApplication {
   (environ: Environ, start_response: StartResponse): Iterable<string>
 }
